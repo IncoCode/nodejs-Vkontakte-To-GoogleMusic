adabd4f8f9d828f3d682d6f9fc1a9b78f1e0bf41
IncoCode <akulich.dimon@gmail.com>
2015-08-01 22:33:00 +0300

    Added ability to enter max count in getUserMusic function

--- vk/vk.js
+++ vk/vk.js
@@ -3,14 +3,17 @@ var Promise = require('bluebird');
 
 module.exports = function VK(accessToken, appId, userId) {
     return {
-        getUserMusic: function () {
+        getUserMusic: function (count) {
+            if (!count)
+                count = 6000;
             return new Promise(function (resolve, reject) {
                 var url = ['https://api.vk.com/method/audio.get?user_id=', userId,
-                    '&v=5.35&access_token=', accessToken]
+                    '&v=5.35&access_token=', accessToken, '&count=', count, '&lang=en']
                     .join('');
                 request(url, function (err, response, body) {
                     if (!err && response.statusCode == 200) {
-                        return resolve(body);
+                        var obj = JSON.parse(body);
+                        return resolve(obj.response.items);
                     }
                     reject(err);
                 })
